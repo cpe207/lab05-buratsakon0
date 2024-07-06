3d06dec982a522faf98ac809c66794eed605727d
buratsakon0 <buratsakon_s@cmu.ac.th>
2024-07-06 10:11:52 +0700

    lab05 completed

--- q2.ts
+++ q2.ts
@@ -1,13 +1,16 @@
 // define interface for Student object
-/* Your code here */
+interface Student {
+  name: string;
+  score: number;
+}
 
 // assign interface/type to the function definition properly
-function findTopNames(students) {
-  /* Your code here */
+function findTopNames(students: Student[]):string[] {
+  return (students.filter((std) => std.score > 8)).map((std) => std.name);
 }
 
 // assign interface/type to the student1 object properly
-const students1 = [
+const students1:Student[] = [
   { name: "john", score: 10 },
   { name: "jane", score: 9 },
   { name: "jim", score: 8 },
